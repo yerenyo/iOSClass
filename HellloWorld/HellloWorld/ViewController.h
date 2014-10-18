fdd83152f83a00deca4826d498d997232a84ebdd
郭栋 <guodong@guodongdeMacBook-Pro.local>
2014-10-18 16:24:59 +0800

    新建HelloWorld

---
+++ HellloWorld/HellloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HellloWorld
+//
+//  Created by 郭栋 on 14-10-18.
+//  Copyright (c) 2014年 郭栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HellloWorld/HellloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HellloWorld
+//
+//  Created by 郭栋 on 14-10-18.
+//  Copyright (c) 2014年 郭栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
